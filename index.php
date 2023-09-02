cc4b8703fbedf4f7cb0c6beecbc7d33cabd22816
RAPHAEL HENDRIGO DE SOUZA GONÇALVES <raphael.hendrigo@gmail.com>
2023-09-02 01:00:22 -0300

    Mensagem descritiva sobre as mudanças feitas 2

--- index.php
+++ index.php
@@ -1,45 +1,45 @@
 <?php
-// // Definir fuso horário
-// date_default_timezone_set('America/Sao_Paulo');
+// Definir fuso horário
+date_default_timezone_set('America/Sao_Paulo');
 
-// // Dados conexão bd local
-// $servidor = 'biblioteca-acervo.mysql.database.azure.com';
-// $banco = 'biblioteca-acervo';
-// $usuario = 'raphael';
-// $senha = '@cervoSJC';
+// Dados conexão bd local
+$servidor = 'biblioteca-acervo.mysql.database.azure.com';
+$banco = 'biblioteca-acervo';
+$usuario = 'raphael';
+$senha = '@cervoSJC';
 
-// // Tentar conectar ao banco de dados
-// try {
-// 	$pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", $usuario, $senha);
-// } catch (PDOException $e) {
-// 	die('Erro ao conectar ao banco de dados!<br>' . $e->getMessage());
-// }
+// Tentar conectar ao banco de dados
+try {
+	$pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", $usuario, $senha);
+} catch (PDOException $e) {
+	die('Erro ao conectar ao banco de dados!<br>' . $e->getMessage());
+}
 
-// // Variáveis globais
-// $nome_sistema = 'Gerenciador de Acervo Tecnico';
-// $email_sistema = 'contato@hugocursos.com.br';
-// $telefone_sistema = '(31)97527-5084';
-// $url_sistema = 'biblioteca-acervo.mysql.database.azure.com';
+// Variáveis globais
+$nome_sistema = 'Gerenciador de Acervo Tecnico';
+$email_sistema = 'contato@hugocursos.com.br';
+$telefone_sistema = '(31)97527-5084';
+$url_sistema = 'biblioteca-acervo.mysql.database.azure.com';
 
-// // Consultar a tabela config
-// $query = $pdo->prepare("SELECT * from config");
-// $query->execute();
-// $res = $query->fetchAll(PDO::FETCH_ASSOC);
-// $linhas = count($res);
+// Consultar a tabela config
+$query = $pdo->prepare("SELECT * from config");
+$query->execute();
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = count($res);
 
-// // Se a tabela estiver vazia, insira um registro padrão
-// if ($linhas == 0) {
-// 	$sql = "INSERT INTO config (nome, email, telefone, logo, logo_rel, icone, marca_dagua, dias_entrega) VALUES (?, ?, ?, 'logo.png', 'logo.jpg', 'icone.png', 'Sim', '5')";
-// 	$stmt = $pdo->prepare($sql);
-// 	$stmt->execute([$nome_sistema, $email_sistema, $telefone_sistema]);
-// } else {
-// 	// Atualize as variáveis globais com os valores do registro retornado
-// 	$nome_sistema = $res[0]['nome'];
-// 	$email_sistema = $res[0]['email'];
-// 	$telefone_sistema = $res[0]['telefone'];
-// 	//... as outras variáveis que você mencionou anteriormente
-// }
-echo "qualquer coisa 2";
+// Se a tabela estiver vazia, insira um registro padrão
+if ($linhas == 0) {
+	$sql = "INSERT INTO config (nome, email, telefone, logo, logo_rel, icone, marca_dagua, dias_entrega) VALUES (?, ?, ?, 'logo.png', 'logo.jpg', 'icone.png', 'Sim', '5')";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([$nome_sistema, $email_sistema, $telefone_sistema]);
+} else {
+	// Atualize as variáveis globais com os valores do registro retornado
+	$nome_sistema = $res[0]['nome'];
+	$email_sistema = $res[0]['email'];
+	$telefone_sistema = $res[0]['telefone'];
+	//... as outras variáveis que você mencionou anteriormente
+}
+
 ?>
 
 <!DOCTYPE html>
